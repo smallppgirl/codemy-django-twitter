5e0a31e2a583e15e80e9c43c25548cd57bbf2a94
smallppgirl <fangzhuang2@gmail.com>
2023-09-19 12:23:52 +0800

    changes to add login

--- musker/views.py
+++ musker/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Profile, Meep
+from .forms import MeepForm
+from django.contrib.auth import authenticate, login, logout
 
 
 def home(request):
     if request.user.is_authenticated:
+        form = MeepForm(request.POST or None)
+        if request.method == "POST":
+            if form.is_valid():
+                meep = form.save(commit=False)
+                meep.user = request.user
+                meep.save()
+                messages.success(request, ("Your Meep Has Been Posted"))
+                return redirect('home')
+
         meeps = Meep.objects.all().order_by("-created_at")
-    return render(request, 'home.html', {"meeps": meeps})
+        return render(request, 'home.html', {"meeps": meeps, "form": form})
+    else:
+        meeps = Meep.objects.all().order_by("-created_at")
+        return render(request, 'home.html', {"meeps": meeps})
 
 
 def profile_list(request):
@@ -38,3 +52,25 @@ def profile(request, pk):
     else:
         messages.success(request, ("You Must Be Logged In To View This Page..."))
         return redirect('home')
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been Logged in"))
+            return redirect('home')
+        else:
+            messages.success(request, ("There was an error logging in, please try again"))
+            return redirect('login')
+    else:
+        return render(request, "login.html", {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You Have Been Logged Out. Sorry"))
+    return redirect('home')
